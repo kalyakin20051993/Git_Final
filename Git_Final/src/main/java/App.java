c13a870b9f9d6ca68bd862e92f4000c94288b59c
Andrey Kalyakin <kalyakin20051993@yandex.ru>
2026-03-02 17:51:56 +0000

    Добавлена функция умножения

--- Git_Final/src/main/java/App.java
+++ Git_Final/src/main/java/App.java
@@ -2,6 +2,10 @@ package ru.yandex.calculator
 
 public class Calculator {
     public static void main(String[] args) {
+        int x;
+        int y;
+        int z;
 
+        z = x * y;
     }
-} 
+}
